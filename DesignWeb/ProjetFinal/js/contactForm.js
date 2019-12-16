48aa650b6b829de6f50bba285449d2f891907f9f
2citron <lambert.samuel@carrefour.cegepvicto.ca>
2019-12-16 14:21:03 -0500

    Push finale 2019

---
+++ DesignWeb/ProjetFinal/js/contactForm.js
@@ -0,0 +1,3 @@
+$('#Sub').click(function () {
+    $('#form').html("<div class=\"container-fluid align-items-center\"><div class=\"row py-6\"></div><div class=\"row\"><div class=\"offset-1 col-10 text-center\"><h1>Formulaire bien envoyé</h1></div></div></div>")
+});
