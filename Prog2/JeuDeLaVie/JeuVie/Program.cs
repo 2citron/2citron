2d3c16742c13578f4db2e6a130a4e619c0d94b0e
2citron <lambert.samuel@carrefour.cegepvicto.ca>
2020-03-11 19:28:48 -0400

    push vie de vie

---
+++ Prog2/JeuDeLaVie/JeuVie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace CegepVicto.TechInfo.H2020.P2.DA1939902.JeuVie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JeuVie JV = JeuVie.GetInstance(100, 30, 25);
+            JV.VoirTransi = false;
+            while (!JV.Stable)
+            {
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine(JV.ToString());
+                JV.FaireTransition();
+                Thread.Sleep(50);
+                //Console.ReadLine();
+            }
+            Console.WriteLine("EQUILIBRIUM!!!");
+            Console.ReadLine();
+        }
+    }
+}
